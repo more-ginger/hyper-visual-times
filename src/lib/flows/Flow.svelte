bbaeb2c40ac37ea8b34578d9ee21231b0795e47b
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-02-26 16:33:54 +0100

    feat: add zero values to data before stack initialization.

--- src/lib/flows/Flow.svelte
+++ src/lib/flows/Flow.svelte
@@ -17,21 +17,61 @@
         key: string
     }
 
-    let { data, month } = $props();
+    let { data, month, globalDomain } = $props();
     
+    function daysInMonth (month:number, year:number) {
+        return new Date(year, month, 0).getDate();
+    }
     // Retrieve the first and last day of the month
     const firstDay = new Date(2024, month - 1, 1);
     const lastDay = new Date(2024, month, 0);
+    const pixelBufferForShorterMonths = (31 - daysInMonth(month, 2024)) * 50
 
-    // Parse data to return valid date and sort them
+    // Parse data to return valid date
     const dataWithParsedDate = data.map((d:baseData) => {
-            return {
-                count: d.count,
-                pub_date: new Date(d.pub_date),
-                keyword: d.keyword,
-                pub_month: d.pub_month
-            }
-        }).sort((a:baseData, b:baseData) => a.pub_date - b.pub_date)
+            
+        const pub_date = new Date(d.pub_date)
+        const year = pub_date.getFullYear()
+        const month = pub_date.getMonth()
+        const day = pub_date.getDate()
+        const date_no_time = new Date(year, month, day);
+
+        return {
+            count: d.count,
+            pub_date: date_no_time,
+            keyword: d.keyword,
+            pub_month: d.pub_month
+        }
+    })    
+
+    // Push 0 values for dates that do not exist in the original dataset
+    const uniqueKeywords = [...new Set(data.map((d:baseData) => d.keyword))]
+    uniqueKeywords.forEach((keyword) => {
+        const currentKeyword = dataWithParsedDate.filter((d: baseData) => d.keyword == keyword)
+        
+        const dateHash = currentKeyword.reduce(function(agg: any, d: any) {
+            agg[d.pub_date] = true;
+            return agg;
+        }, {})
+
+        d3.timeDay.range(firstDay, lastDay)
+        .filter(function(date: Date){
+            return !dateHash[date.toString()]
+        })
+        .forEach(function(date: Date) {
+            var emptyRow = { 
+                count: 0,
+                pub_date: date,
+                keyword: keyword,
+                pub_month: month
+                };
+
+            dataWithParsedDate.push(emptyRow);
+        });
+    })
+
+    // sort data based on date
+    dataWithParsedDate.sort((a:baseData, b:baseData) => a.pub_date - b.pub_date)
 
     // Generates stack for area charts
     const uniqueKeysForStack = d3.union(dataWithParsedDate.map((d:baseData) => d.keyword))
@@ -44,13 +84,18 @@
     const stackedData = stack(d3.index(dataWithParsedDate, (d: baseData) => d.pub_date, (d: baseData) => d.keyword))
     ////
 
+    
     ////
     // Scales and constructors
     const domainXscale = [firstDay, lastDay]
-    const domainYScale = d3.extent(stackedData.flat(2))
     
-    const xScale = $derived(d3.scaleLinear().range([0, visWidth]).domain(domainXscale))
-    const yScale = d3.scaleLinear().range([0, 300]).domain(domainYScale)
+    const xScale = $derived(d3.scaleLinear().range([0, visWidth - pixelBufferForShorterMonths]).domain(domainXscale))
+    const yScale = d3.scaleLinear().range([0, 300]).domain(globalDomain)
+
+    const xAxis = $derived(xScale.ticks())
+    const yAxis = yScale.ticks(5)
+
+
     const area = $derived(d3.area()
         .x((d:HybridArray) => xScale(d.data[0]))
         .y0((d:NestedHybridArray) => 300 - yScale(d[0]))
@@ -60,27 +105,54 @@
     
     ////
     // Computed data, ready to render
-    const computedFlowData = $derived(stackedData.map((group:NestedHybridArray) => {
-        return {
-            area: area(group),
-            keyword: group.key
-        }
-    }))
+    const computedFlowData = $derived(
+        stackedData.map((group:NestedHybridArray) => {
+            return {
+                area: area(group),
+                keyword: group.key
+            }
+        })
+    )
 
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
-<main>
+<main class="border-t border-2 border-black">
     <div>{month}</div>
-    <svg width={visWidth} height="300" class="m-auto my-[-50px] ">
+    <svg width={visWidth} height="300" class="m-auto">
+        <g>
+            {#each xAxis as tick}
+                <line x1={xScale(tick)} x2={xScale(tick)} y1={300} y2={0} stroke="black"/>
+            {/each}
+         </g>
+         <g>
+            {#each yAxis as tick}
+                <line x1="0" x2={innerWidth - pixelBufferForShorterMonths} y1={yScale(tick)} y2={yScale(tick)} stroke="black"/>
+            {/each}
+         </g>
         <g>
             {#each computedFlowData as flow}
-                <path d={flow.area} fill="white" stroke="#9494b8" id={flow.keyword}/>
+                <path 
+                    d={flow.area} 
+                    fill="white" 
+                    stroke="#9494b8" 
+                    id={flow.keyword}
+                />
             {/each}
         </g>
         <g>
-            <line x1={xScale(firstDay)} x2={xScale(lastDay)} y1={300} y2={300} stroke="black" stroke-width="2"/>
+
         </g>
+        <!-- <g>
+            <line 
+                x1={xScale(firstDay)} 
+                x2={xScale(lastDay)} 
+                y1={300} 
+                y2={300} 
+                stroke="black" 
+                stroke-width="2"
+            />
+        </g> -->
     </svg>
 </main>
--- src/lib/flows/FlowVis.svelte
+++ src/lib/flows/FlowVis.svelte
@@ -1,20 +1,43 @@
 <script lang="ts">
     import type { baseData } from '../../types';
     import Flow from "./Flow.svelte";
+    // @ts-expect-error
+    import * as d3 from "d3";
+
 
     let { data } = $props();
-
-    const filteredData: baseData[] = $state(data.filter((d: baseData) => d.section_name == "Movies"));
-    const groupedData = $state(Object.groupBy(filteredData, (d: baseData) => d.pub_month));
+    const filterCategory = "World"
+    const filteredData: baseData[] = $state(data.filter((d:baseData) => d.section_name == filterCategory));
+    const groupedData: Partial<Record<number, baseData[]>> = $state(Object.groupBy(filteredData, (d:baseData) => d.pub_month));
     const monthsForFlows: number[] = $state(Object.keys(groupedData).map(Number))
     
+    // I define a global domain to have a even representation of the coverage throughout the months
+    const maxValueForDomain: number[] = []
+    
+    Object.keys(groupedData).forEach((key) => {
+        const numKey = Number(key)
+        const currentMonthData = groupedData[numKey]
+       
+        if (!currentMonthData) return;
+
+        const GroupByDay = Object.groupBy(currentMonthData, (d:baseData) => d.pub_date)
+
+        Object.keys(GroupByDay).forEach((day) => {
+            const dayKey = Number(day)
+            const maxDailyValue = d3.sum(GroupByDay[dayKey]?.map((d:baseData) => d.count))
+            maxValueForDomain.push(maxDailyValue)
+        }) 
+    })
+    
+    const globalDomain = [0, d3.max(maxValueForDomain)]
+
 </script>
 
 <div>
-    <h1>Flows vis</h1>
+    <h1>Flows vis about {filterCategory}</h1>
     <div>
         {#each monthsForFlows as Month}
-            <Flow month={Month} data={groupedData[Month]}></Flow>
+            <Flow month={Month} data={groupedData[Month]} globalDomain={globalDomain}></Flow>
         {/each}
     </div>
 </div>
