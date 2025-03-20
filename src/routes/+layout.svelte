5a0c41e84d09978d0485621edd4e2b2d633a92e2
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-03-20 11:16:19 +0100

    feat: new nav menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,51 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+	let isDropdownOpen = $state(false);
+
+	const handleDropdownClick = () => {
+    isDropdownOpen = !isDropdownOpen // togle state on click
+  }
+
+  const handleDropdownFocusLoss = (event: FocusEvent) => {
+	const { relatedTarget, currentTarget } = event;
+    // use "focusout" event to ensure that we can close the dropdown when clicking outside or when we leave the dropdown with the "Tab" button
+	if (relatedTarget instanceof HTMLElement 
+		&& currentTarget instanceof HTMLElement 
+		&& currentTarget.contains(relatedTarget)) return
+    
+	isDropdownOpen = false
+  }
 </script>
 
-<div class="w-full px-4 bg-green-100">
-<nav class="w-full px-4 mt-4 border-b flex justify-between">
-	<div class="w-1/2 bg-red-100">
+<div class="w-full px-4 max-w-desktop-size m-auto">
+<nav class="w-full px-4 pb-2 mt-4 border-b flex justify-between">
+	<div class="w-1/2">
 		<a href="/">
 			<p>Panopticum News</p>
 		</a>
 	</div>
 	<div>
-		<div>
-			<button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" data-dropdown-placement="right-start" type="button">Menu placeholder</button>
-			<div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
-				<ul>
-					<li>
-						<a href="/places">Chapter 1: Places</a>
-					</li>
-					<li>
-						<a href="/">Chapter 2: People</a>
-					</li>
-					<li>
-						<a href="/">Chapter 3: Discourse</a>
-					</li>
-				</ul>
+		<div onfocusout={handleDropdownFocusLoss} class="relative inline-block text-left">
+			<div>
+			  <button onclick={handleDropdownClick} type="button" class="inline-flex w-full justify-center gap-x-1.5 px-3 text-sm font-semibold cursor-pointer" id="menu-button" aria-expanded="true" aria-haspopup="true">
+				Chapters
+				<svg width="46" height="21" viewBox="0 0 46 21" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" data-slot="icon">
+					<circle cx="35" cy="10.5" r="10" fill="black" stroke="#fcf4e9" stroke-width="2"/>
+					<circle cx="23" cy="10.5" r="10" fill="black" stroke="#fcf4e9"/>
+					<circle cx="11" cy="10.5" r="10" fill="black" stroke="#fcf4e9"/>
+				</svg>					
+			  </button>
 			</div>
-		</div>
+			<div style:visibility={isDropdownOpen ? 'visible' : 'hidden'} class="absolute top-10 right-0 mt-2 z-10 mt-2 w-56  border bg-ivory-default rounded-md ring-1 ring-black/5 focus:outline-hidden" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+			  <div class="py-1" role="none">
+				<!-- Active: "bg-gray-100 text-gray-900 outline-hidden", Not Active: "text-gray-700" -->
+				<a href="/places" class="block px-4 py-2 hover:underline" role="menuitem" tabindex="-1" id="menu-item-0">Chapter 1: Places</a>
+				<a href="/" class="block px-4 py-2" role="menuitem" tabindex="-1" id="menu-item-1">Chapter 2: Images</a>
+				<a href="/" class="block px-4 py-2" role="menuitem" tabindex="-1" id="menu-item-2">Chapter 3: Text</a>
+			  </div>
+			</div>
+		  </div>
 	</div>
 
 </nav>
--- src/routes/places/+page.svelte
+++ src/routes/places/+page.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
-import FlowVis from "$lib/flows/FlowVis.svelte";
-
 //this export gets the data from the +page.server page
-export let data: { flowData: Record<string, any> }
+
 
 </script>
 <div class="base">
-    {#await data.flowData}
-            <div>Waiting</div>
-        {:then flowData}
-            <FlowVis data={flowData}/> 
-    {/await}
+    <div>Places</div>
 </div>
