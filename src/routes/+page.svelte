1f67aa93c74fbbcc737983208b95db65cd2999dc
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-03-20 11:23:12 +0100

    feat: basic structure of initial page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,30 @@
+<script lang="ts">
+    import MainIllustration from "$lib/essay/MainIllustration.svelte";
+
+</script>
 <main>
     <div class="base">
-        <h1>Basic scaffolding for Hyper Visual Times</h1>
-        <p>This is an extremely WIP web app with the different chapters of the HVT project. Each page is a different visualization.</p>
+        <div id="essay-container" class="w-full flex my-10 bg-red-100">
+            <div class="w-3/4">
+                <MainIllustration/>
+            </div>
+            <div class="w-1/4">
+                <div>
+                    <h1>Main title of the essay</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tellus enim, feugiat in lobortis id, luctus in dolor. Fusce a eleifend odio. Aliquam at massa in ligula posuere tempus. 
+                        Vivamus mollis lacus posuere, maximus ligula id, dictum libero. Proin blandit laoreet convallis. Quisque pellentesque nunc leo, at pretium lacus cursus eu. Suspendisse sit amet neque in massa tincidunt consequat. 
+                        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras non urna vitae mauris dignissim laoreet vitae a ligula.
+                        Praesent rhoncus sed risus in tempus. Vestibulum in blandit neque. Sed eget neque risus. Curabitur sollicitudin vulputate eros. Aenean dignissim gravida viverra. 
+                        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis non purus in lacus cursus maximus ac in tellus.
+                        Suspendisse bibendum nisi quis libero scelerisque, sed pharetra nunc vestibulum. Maecenas et facilisis tellus. Duis sit amet eleifend leo, et semper nisi. 
+                        Vestibulum sed nisi facilisis, suscipit ante vitae, cursus odio. Aliquam sit amet nibh non est aliquam venenatis. Aenean vitae mi urna. 
+                        Quisque vitae erat cursus, ultricies metus non, commodo nisl. Duis eu erat a nulla accumsan accumsan sit amet quis nulla. Nulla facilisi. 
+                        Vestibulum iaculis dignissim nibh, at tempor mi luctus nec. Proin dignissim lacus ut tristique tempus. Nulla id scelerisque libero. Morbi sem massa, congue ut aliquet non, dapibus id enim. 
+                        In in ipsum vel dui porttitor dapibus non eget leo. In elementum ligula non lacus malesuada, pretium laoreet ante accumsan.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
