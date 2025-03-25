ffa981ca389c991766eab2f6216043605bd0f4f8
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-03-25 15:39:49 +0100

    feat: add dynamic footnote position in essay

--- src/lib/places/IntroductionText.svelte
+++ src/lib/places/IntroductionText.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import markedFootnote from "marked-footnote";
 import { marked } from "marked";
+import { setFootnotes } from "$lib/utils/actions.svelte";
 import places_essay from "../../../static/text/places-essay.md?raw";
 let shown = $state(true);
 
@@ -8,13 +9,14 @@ marked.use({async: true, breaks:true}, markedFootnote())
 const essayContent = marked.parse(places_essay)
 </script>
 
-<div class="w-full md:w-3/7 m-auto">
+<div>
     {#await essayContent}
     <div>loading</div>
     {:then essayContent} 
+    <div class="w-full md:w-3/7 m-auto" use:setFootnotes>
         <div class="m-10 essay" class:optional-hidden={!shown}>
             {@html essayContent}
         </div>
+        </div>
     {/await}
-
 </div>
---
+++ src/lib/utils/actions.svelte.ts
@@ -0,0 +1,61 @@
+// The action is used to assign a dynamic position to in-text footnotes
+// The approach is currently a bit basic, I just get the position of a reference in text and assign
+// the top position to a footnote that has position absolute. This way, the footnote is moved to the right of its reference.
+
+export function setFootnotes(node:HTMLDivElement) {
+    // get section where footnotes are included
+    const footNoteSection = node.querySelector(".footnotes");
+    // get rect of parent to calculate the footnotes relative position
+    const parentRect = node.getBoundingClientRect();
+    
+    if (footNoteSection) {
+        // get individual footnotes as nodes
+        const footnotesNodes = footNoteSection.querySelectorAll("li")
+        // iterate over footnotes
+        if (footnotesNodes) {
+            for (let i = 0; i < footnotesNodes.length; i++) {
+                // get the current footnote
+                const currentNode = footnotesNodes[i]
+                // get previous one
+                const previousNode = footnotesNodes[i - 1]
+                // get linked ref in text
+                const inTextRef = node.querySelector(`#footnote-ref-${i + 1}`)
+                // if the ref is not null, get the rect
+                const boundingRectOfRef = inTextRef?.getBoundingClientRect()
+                
+                // here we calc the relative position of ref to the container, 
+                // so that the footnotes will be at fixed pos
+                const relativeRect = {
+                    top: Math.max((boundingRectOfRef?.top ?? 0) - parentRect.top, 180),
+                    left: (boundingRectOfRef?.left ?? 0) - parentRect.left,
+                    bottom: (boundingRectOfRef?.bottom ?? 0) - parentRect.bottom,
+                    right: (boundingRectOfRef?.right ?? 0) - parentRect.right
+                }
+
+                // get the reference position on page
+                const YOfRef = relativeRect?.top ?? 0
+
+                // get the rect of the previous footnote
+                const boundingRectOfPrevNode = previousNode 
+                    ? previousNode.getBoundingClientRect() 
+                    : null
+                
+                // get the y pos of the previous footnote
+                const yOfPreviousNode = boundingRectOfPrevNode 
+                    ? boundingRectOfPrevNode.y 
+                    : 0
+                // get the height of the previous footnote
+                const hOfPreviousNode = boundingRectOfPrevNode 
+                    ? boundingRectOfPrevNode.height 
+                    : 0
+                // assigns top property to current footnote, 
+                // to avoid overlap we calc if we should add a buffer to the footnote
+                currentNode.style.top = YOfRef 
+                    + (yOfPreviousNode + hOfPreviousNode > YOfRef ? hOfPreviousNode : 0) 
+                    + "px"
+            }
+        }
+
+    }
+   
+}
