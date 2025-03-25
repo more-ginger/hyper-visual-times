772cf2c7fc81d3c64360e9e1f91288ee7c849087
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-03-25 12:58:31 +0100

    feat: add async call to markdown and footnote handling

--- src/lib/places/IntroductionText.svelte
+++ src/lib/places/IntroductionText.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
+import markedFootnote from "marked-footnote";
 import { marked } from "marked";
 import places_essay from "../../../static/text/places-essay.md?raw";
 let shown = $state(true);
 
-
+marked.use({async: true, breaks:true}, markedFootnote())
+const essayContent = marked.parse(places_essay)
 </script>
 
 <div class="w-full md:w-3/7 m-auto">
-    <div class="m-10 essay" class:optional-hidden={!shown}>
-        {@html marked(places_essay)}
-    </div>
+    {#await essayContent}
+    <div>loading</div>
+    {:then essayContent} 
+        <div class="m-10 essay" class:optional-hidden={!shown}>
+            {@html essayContent}
+        </div>
+    {/await}
+
 </div>
