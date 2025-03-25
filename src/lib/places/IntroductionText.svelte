f6b1e2307efa44dbdeee1efe2b9835d669ed7cfd
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-03-25 12:26:23 +0100

    feat: add markdown render for essay components

---
+++ src/lib/places/IntroductionText.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { marked } from "marked";
+import places_essay from "../../../static/text/places-essay.md?raw";
+let shown = $state(true);
+
+
+</script>
+
+<div class="w-full md:w-3/7 m-auto">
+    <div class="m-10 essay" class:optional-hidden={!shown}>
+        {@html marked(places_essay)}
+    </div>
+</div>
