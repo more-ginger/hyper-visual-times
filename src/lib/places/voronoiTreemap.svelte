ae115136a4c86072074d8361ff8119674b38ef00
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-03-26 16:33:44 +0100

    feat: voronoi treemap

---
+++ src/lib/places/voronoiTreemap.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+    // @ts-expect-error
+    import { voronoiTreemap } from "d3-voronoi-treemap";
+    // @ts-expect-error
+    import { hierarchy } from "d3-hierarchy";
+    
+    let { regionData } = $props();
+    let fullWidth = $state(10); 
+    let fullHeight = $state(10);
+    let leaves = $state([]);
+    let descendants = $state([]);
+    let smallestSide = $derived(Math.min(fullWidth, fullHeight))
+
+    let regionColors = {
+        "Africa": "#FF5733",
+        "Asia": "#33FF57",
+        "Europe": "#3357FF",
+        "North America": "#FF33A1",
+        "South America": "#A133FF",
+        "Oceania": "#33FFF5",
+        "Antarctica": "#F5FF33"
+    };
+
+    function computeCirclingPolygon(radius:number) {
+        var points = 60,
+            increment = 2*Math.PI/points,
+            circlingPolygon = [];
+        
+        for (var a=0, i=0; i<points; i++, a+=increment) {
+          circlingPolygon.push(
+            [radius + radius*Math.cos(a), radius + radius*Math.sin(a)]
+        )}
+        
+        return circlingPolygon
+    }
+
+    let circlingLayout = $derived(computeCirclingPolygon(smallestSide / 2))
+    var voronoiClip = $derived(voronoiTreemap().clip(circlingLayout)); // sets the clipping polygon
+
+    function weightAccessor(d:any) {
+        return d.count;
+    }
+
+    var rootNode = $derived(hierarchy(regionData).sort((a, b) => b.count - a.count || b.count - a.count).sum(weightAccessor))
+    
+    $effect(() => {
+        voronoiClip(rootNode);
+        leaves = rootNode.leaves()
+        descendants = rootNode.descendants()
+        $inspect(rootNode.descendants())
+    })
+
+   
+
+</script>
+<div class="h-full">
+    <div>
+        <div class=border-b><h4>Areas of interests</h4></div>
+        <div><p>Subtitle</p></div>
+    </div>
+    <div class="h-9/10">
+        <svg 
+            width="100%" 
+            height="100%"
+            bind:clientWidth={fullWidth} 
+            bind:clientHeight={fullHeight}    
+            class="border"
+        >
+            {#if leaves}
+                {#each descendants as leave}
+                    <path 
+                        d={"M"+(leave.polygon.join(",") || "")+"z"} 
+                        fill="transparent"
+                        stroke="white"
+                        stroke-width={leave.height + 1 % 2}
+                        rx=100
+                    />
+                {/each}
+            {/if}
+
+        </svg>
+    </div>
+</div>
--- src/routes/places/+page.server.ts
+++ src/routes/places/+page.server.ts
@@ -3,8 +3,10 @@ import type { PageServerLoad } from './$types';
 import type { countryDataType } from '../../types';
 
 export const load: PageServerLoad = async () => {
-    const github_url = "https://raw.githubusercontent.com/more-ginger/pn-analysis/refs/heads/place-analysis/data/places/coverage_by_country.csv"
-    const response = await fetch(github_url);
+    const coverage_by_country_url = "https://raw.githubusercontent.com/more-ginger/pn-analysis/refs/heads/place-analysis/data/places/coverage_by_country.csv"
+    
+    const response = await fetch(coverage_by_country_url);
+    
     if (!response.ok) {
         throw new Error(`Failed to load data: ${response.statusText}`)
     }
@@ -12,8 +14,19 @@ export const load: PageServerLoad = async () => {
     const csvText = await response.text();
     const parsedCsv = Papa.parse(csvText, {header:true}).data as countryDataType[];
 
+    const coverage_by_region_url = "https://raw.githubusercontent.com/more-ginger/pn-analysis/refs/heads/place-analysis/data/places/coverage_by_region.json"
+    
+    const coverage_by_region_response = await fetch(coverage_by_region_url);
+    
+    if (!coverage_by_region_response.ok) {
+        throw new Error(`Failed to load data: ${response.statusText}`)
+    }
+
+    const jsonData = await coverage_by_region_response.json();
+    console.log(jsonData)
+
     return {
-        data:parsedCsv.map((d:countryDataType) => {
+        data: parsedCsv.map((d:countryDataType) => {
             return {
                 country: d.country,
                 coords: [+d.Latitude, +d.Longitude],
@@ -21,6 +34,7 @@ export const load: PageServerLoad = async () => {
                 count_nyt: +d.count_of_articles_nyt,
                 iso: d.iso_alpha3,
             }
-        })
+        }),
+        coverageByRegion: jsonData
     }
 }
