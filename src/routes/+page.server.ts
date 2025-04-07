925afe3f1fedf84d51b00738df2214d27ccd3979
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-04-07 18:01:28 +0200

    feat: add zeit and nyt sample to MainIllustration

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,11 @@
 import { supabase } from "$lib/supabaseClient";
 
 export async function load() {
-  const { data } = await supabase.from("nyt_articles").select();
+  const { data:nyt_all } = await supabase.from("nyt_articles").select();
+  const { data:zeit_all } = await supabase.from("zeit_articles").select();
+
   return {
-    articles: data ?? [],
+    nyt_all: nyt_all ?? [],
+    zeit_all: zeit_all ?? []
   };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
     <div class="w-11/12 m-auto">
         <div id="essay-container" class="w-full flex my-4">
             <div class="md:w-6/9 sticky h-dvh top-50 md:top-0">
-                <MainIllustration/>
+                <MainIllustration data={data}/>
             </div>
             <div class="md:w-3/9">
                 <div>
