89412f6d739c1d2a98a2cbab04b0381e591a0389
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-04-30 16:28:30 +0200

    feat: implement basic diagram layout for countries comparison

--- src/lib/places/VennDiagram.svelte
+++ src/lib/places/VennDiagram.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import Dropdown from "$lib/common/Dropdown.svelte";
+	import { text } from "@sveltejs/kit";
     import type { countryDataType } from '../../types';
+    import { layout } from "@upsetjs/venn.js";
 
     let {data} = $props()
     
@@ -12,9 +14,8 @@
 
     let firstCountrySelected = $state(dropdownData[0])
     let secondCountrySelected = $state(dropdownData[1])
-    let selectedCountries = $state<string[]>([])
-    let results = $state([])
-
+    let thirdCountrySelected = $state(dropdownData[2])
+    let selectedCountries = $state<any[]>([])
 
     function toggleSelection(countries: string[]) {
         const selectedDataPoints = countries.map((c) => {
@@ -24,21 +25,80 @@
         selectedCountries = [...selectedDataPoints]
     }
 
+    let results = $derived.by(
+        () => {
+            const sets = []
+            for (let i = 0; i < selectedCountries.length; i++) {
+                for (let j = i; j < selectedCountries.length; j++) {
+                    const a = selectedCountries[i];
+                    const b = selectedCountries[j];
+                    if (!a || !b) continue;
+                    const aKeywords = new Set(Object.keys(a.keywords_nyt))
+                    const bKeywords = new Set(Object.keys(b.keywords_nyt))
+                    const shared = [...aKeywords].filter(k => bKeywords.has(k));
+    
+                    if (shared.length > 0) {
+                        sets.push({
+                            sets: a.country === b.country 
+                                ? [a.country] 
+                                : [a.country, b.country],
+                            size: shared.length,
+                            shared
+                        })
+                    }
+                }
+            }
+
+            return sets.sort((a, b) => b.size - a.size)
+        }
+    )
+
     $effect(() => {
         toggleSelection(
-            [firstCountrySelected.key, 
-            secondCountrySelected.key
-        ])
+            [
+                firstCountrySelected.key, 
+                secondCountrySelected.key,
+                thirdCountrySelected.key
+            ]
+        )
     })
 
+    const diagram = $derived(selectedCountries.length > 0 ? layout(results) : null)
+    $inspect(diagram)
     
-
 </script>
-<div>
+<div class="w-full">
     <div>
-          First country: <Dropdown availableFilter={dropdownData} bind:selected={firstCountrySelected}/>
+          First country: 
+          <Dropdown 
+            availableFilter={dropdownData} 
+            bind:selected={firstCountrySelected}
+        />
     </div>
     <div>
-        Second country: <Dropdown availableFilter={dropdownData} bind:selected={secondCountrySelected}/>
-  </div>
+        Second country: 
+        <Dropdown 
+            availableFilter={dropdownData} 
+            bind:selected={secondCountrySelected}
+        />
+    </div>
+    <div>
+        Second country: 
+        <Dropdown 
+            availableFilter={dropdownData} 
+            bind:selected={thirdCountrySelected}
+        />
+    </div>
+  <svg width="100vw" height="100vh">
+    {#if diagram}
+        {#each diagram as item}
+            <path d={item.path} fill="none" stroke="black" stroke-width="2" />
+            {#if item.data.sets.length <= 1}
+                {#each item.data.sets as country}
+                    <svg:text x={item.text.x} y={item.text.y} text-anchor="middle">{country}</svg:text>
+                {/each}
+            {/if}
+        {/each}
+    {/if}
+  </svg>
 </div>
