78211ae991698c31a0b695480549008f871e5479
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-05-22 16:31:43 +0200

    style: dummy text to simulate scroll in homepage

--- src/lib/essay/MainIllustration.svelte
+++ src/lib/essay/MainIllustration.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import ArticlesColumn from "./ArticlesColumn.svelte";
-    let { data } = $props();
+    let { data, step } = $props();
     
     let shuffledZeitArticles = $derived(data.zeit_all.sort(() => 0.5 - Math.random()))
     let randomZeitSelection = $derived(shuffledZeitArticles.slice(0, 4));
@@ -16,21 +16,31 @@
     }))
 </script>
 <figure class="w-full">
+    {#if step === 0}
     <div class="w-full h-dvh flex">
         {#each dataSampleForRow as articlesPair, a}
             <ArticlesColumn data={articlesPair} index={a + 1}/>
         {/each}
     </div>
+    {/if}
+    {#if step === 1}
     <div>
         <img src="img/pn-news-illustration.svg"/>
     </div>
+    {/if}
+    {#if step === 2}
     <div>
         <img src="img/pn-news-illustration_step2.svg"/>
     </div>
-        <div>
+    {/if}
+    {#if step === 3}
+    <div>
         <img src="img/pn-news-illustration_step3.svg"/>
     </div>
-        <div>
+    {/if}
+    {#if step === 4}
+    <div>
         <img src="img/pn-news-illustration_step4.svg"/>
     </div>
+    {/if}
 </figure>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,6 @@
     const scroller = scrollama();
     let step = $state(0);
     let essayIsRendered = $state(false);
-    let essayContainer = $state<HTMLDivElement | null>(null);
     
     function onEssayRender(data:{isEssayRendered:boolean}) {
         essayIsRendered = data.isEssayRendered
@@ -34,41 +33,21 @@
             }) 
         }
     })
-    
-    // $effect(() => {
-    //     console.log("in effect", essayContainer?.getElementsByClassName("step").length)
-    //     if (essayContainer?.getElementsByClassName("step").length !== 0) {
-    //         console.log("!")
-    //         scroller
-    //         .setup({
-    //             step: ".step",
-    //             debug: false,
-    //             offset: 0.7
-    //         })
-    //         .onStepEnter((response) => {
-    //             step = response.index
-    //             console.log("step", step)
-    //             // { element, index, direction }
-    //         })
-    //         .onStepExit((response) => {
-    //             //
-    //         }) 
-    //     }
-
-    // })
-
 </script>
+
 <main>
     <div class="w-11/12 m-auto z-0">
         <div id="essay-container" class="w-full flex">
-            <div class="md:w-6/9 sticky top-50 md:top-0">
-                <MainIllustration data={data}/>
+            <div class="md:w-6/9 sticky h-dvh top-0 md:top-0">
+                <MainIllustration data={data} step={step}/>
             </div>
-            <div class="md:w-3/9">
-                <div bind:this={essayContainer}>
+            <div class="md:w-3/9 relative">
+                <div>
                     <BlocksRenderer rawtext={rawIntroText} {onEssayRender}/>
                 </div>
             </div>
         </div>
     </div>
 </main>
+
+
