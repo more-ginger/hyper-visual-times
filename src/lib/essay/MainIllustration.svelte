5174ea64b06244d615ee346d61b124ea690b192e
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-05-22 17:11:31 +0200

    refactor: structure of the essay for better content simulation

--- src/lib/essay/MainIllustration.svelte
+++ src/lib/essay/MainIllustration.svelte
@@ -8,13 +8,15 @@
     let shuffledNYTArticles = $derived(data.nyt_all.sort(() => 0.5 - Math.random()))
     let randomNYTSelection = $derived(shuffledNYTArticles.slice(0, 4));
 
-    let dataSampleForRow = $derived(randomNYTSelection.map((article: { /* define the article type here */ }, a: number) => {
+    let dataSampleForRow = $derived(
+        randomNYTSelection.map((article: { /* define the article type here */ }, a: number) => {
         return {
             nyt: article,
             zeit: randomZeitSelection[a]
         }
     }))
 </script>
+
 <figure class="w-full">
     {#if step === 0}
     <div class="w-full h-dvh flex">
@@ -25,22 +27,22 @@
     {/if}
     {#if step === 1}
     <div>
-        <img src="img/pn-news-illustration.svg"/>
+        <img src="img/pn-news-illustration.svg" class="my-10"/>
     </div>
     {/if}
     {#if step === 2}
     <div>
-        <img src="img/pn-news-illustration_step2.svg"/>
+        <img src="img/pn-news-illustration_step2.svg" class="my-10"/>
     </div>
     {/if}
     {#if step === 3}
     <div>
-        <img src="img/pn-news-illustration_step3.svg"/>
+        <img src="img/pn-news-illustration_step3.svg" class="my-10"/>
     </div>
     {/if}
     {#if step === 4}
     <div>
-        <img src="img/pn-news-illustration_step4.svg"/>
+        <img src="img/pn-news-illustration_step4.svg" class="my-10"/>
     </div>
     {/if}
 </figure>
