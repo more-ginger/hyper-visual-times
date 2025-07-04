6fd9ec12033249f9217b2aecb6e22c7679c81d10
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-07-04 09:42:31 +0200

    refactor: remove legacy tile components

--- src/lib/essay/ArticlesRow.svelte
+++ src/lib/essay/ArticlesRow.svelte
@@ -11,13 +11,13 @@
             src = "img/placeholder-nyt.png"
         }        
     }
-    
+
 </script>
 <div 
     class={`relative bg-white/75 backdrop-blur-xs rounded-sm border m-2 p-2 ${a % 2 === 0 ? "delay-150 animate-bounce-two" : "delay-700 animate-bounce"}`}  
     style="transform: translateY({randomIntTranslate}%)"
     >
-        <div class="overflow-hidden max-h-55 w-full">
+        <div class="overflow-hidden max-h-45 w-full">
             <img 
                 class="mb-2 w-full" 
                 src={src} 
