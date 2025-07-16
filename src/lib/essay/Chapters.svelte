03b54f29b0c76533c88abf882164dce4d0d73223
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-07-16 16:02:43 +0200

    feat: new outline for chapters

--- src/lib/essay/Chapters.svelte
+++ src/lib/essay/Chapters.svelte
@@ -1,16 +1,20 @@
 <div>
-	<h1 class="pb-4 text-2xl md:text-5xl">The Hyper-visual Times</h1>
-	<div class="mb-20">
-		After building a partial blueprint of the material layer of news, the chapters included in this
-		interactive essay will dive into one particular dimension of digital news-making. The goal is to
-		reverse the transient and ephemeral nature of digital news through the creation of distinct
-		pictures and snapshots of news coverage. These snapshots compose a wider horizon for digital
-		news consumption with editorial and platform-related strategies coming to the foreground in
-		showing and comparing the rhetorical and contextual rules of selected news outlets.
+	<div class="m-auto w-6/12">
+		<h1 class="pb-4 text-2xl md:text-5xl">The Hyper-visual Times</h1>
+		<div class="mb-20">
+			After building a partial blueprint of the material layer of news, the chapters included in
+			this interactive essay will dive into one particular dimension of digital news-making. The
+			goal is to reverse the transient and ephemeral nature of digital news through the creation of
+			distinct pictures and snapshots of news coverage. These snapshots compose a wider horizon for
+			digital news consumption with editorial and platform-related strategies coming to the
+			foreground in showing and comparing the rhetorical and contextual rules of selected news
+			outlets.
+		</div>
 	</div>
-	<div>
-		<div class="mb-20 md:grid md:grid-cols-3">
-			<div class="m-2 my-20 cursor-pointer">
+	<div class="m-auto w-6/12">
+		<div class="mb-20">
+			<div class="my-4 cursor-pointer">
+				<h2 class="mb-2 font-serif text-xl">The geo-politics of news</h2>
 				<div class="flex cursor-pointer">
 					<div class="h-[25px] w-[25px] rounded-2xl bg-black px-2 text-center text-white">1</div>
 					<div class="ml-2 flex rounded-xl border px-2">
@@ -20,17 +24,17 @@
 						Available
 					</div>
 				</div>
-				<img class="mt-2 pr-4" src="img/places_header.svg" />
-				<h3 class="my-2 pb-0">The geo-politics of news</h3>
-				<div class="mb-2">
+				<div class="my-2">
 					This chapter focuses on how geographic keywords are used across the online coverage of two
 					newspapers: The New York Times and Zeit Online. First, keywords that are related to
 					geographic locations are extracted and preprocessed to reach a common standard for both
 					publications. Then, other keywords related to these countries are extracted to investigate
 					the overlaps and differences shown by the two newspapers in covering geo-political issues.
 				</div>
+				<img class="m-auto my-2" src="img/places_header.svg" />
 			</div>
 			<div class="m-2 my-20">
+				<h2 class="mb-2 font-serif text-xl">Discoursive interrelations</h2>
 				<div class="flex">
 					<div class="h-[25px] w-[25px] rounded-2xl bg-black px-2 text-center text-white">2</div>
 					<div class="ml-2 flex rounded-xl border px-2">
@@ -38,15 +42,17 @@
 						Coming soon
 					</div>
 				</div>
-				<h3 class="my-2 pb-0">Discoursive interrelations</h3>
-				Using machine learning and topic modeling techniques, key terms have been extracted from one
-				year worth of articles from The New York Times archives. As a result, we generated clusters of
-				similar words showing how often certain topics are discussed throughout the newspaper coverage.
-				In this chapter, these clusters have been visualized to allow for analyzing the relationships
-				between different themes.
-				<img class="mt-2 pr-4" src="img/texts_header.svg" />
+				<div class="my-2">
+					Using machine learning and topic modeling techniques, key terms have been extracted from
+					one year worth of articles from The New York Times archives. As a result, we generated
+					clusters of similar words showing how often certain topics are discussed throughout the
+					newspaper coverage. In this chapter, these clusters have been visualized to allow for
+					analyzing the relationships between different themes.
+				</div>
+				<img class="m-auto my-2" src="img/texts_header.svg" />
 			</div>
 			<div class="m-2 my-20">
+				<h2 class="mb-2 font-serif text-xl">The language of images</h2>
 				<div class="flex">
 					<div class="h-[25px] w-[25px] rounded-2xl bg-black px-2 text-center text-white">3</div>
 					<div class="ml-2 flex rounded-xl border px-2">
@@ -54,13 +60,14 @@
 						Coming soon
 					</div>
 				</div>
-				<img class="mt-2 pr-4 grayscale" src="img/images_header.png" />
-				<h3 class="my-2 pb-0">The language of images</h3>
-				This chapter focuses on images of people as cultural artefacts built using long-established conventions
-				of visual depictions. By extracting facial features from one year Zeit Online and The New York
-				Times coverage, only images depicting famous people (politicians, actors, etc.) have been retained.
-				The visualizations show how frequently certain subjects are shown and what rhetorical devices
-				are applied in depicting them.
+				<div class="my-2">
+					This chapter focuses on images of people as cultural artefacts built using
+					long-established conventions of visual depictions. By extracting facial features from one
+					year Zeit Online and The New York Times coverage, only images depicting famous people
+					(politicians, actors, etc.) have been retained. The visualizations show how frequently
+					certain subjects are shown and what rhetorical devices are applied in depicting them.
+				</div>
+				<img class="m-auto my-2 w-92 grayscale" src="img/images_header.png" />
 			</div>
 		</div>
 	</div>
