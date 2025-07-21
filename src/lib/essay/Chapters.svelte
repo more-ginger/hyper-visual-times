482259c4d12ad3a5f7b308629f886a499b61a2da
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-07-21 11:33:24 +0200

    feat: new tiles interface

--- src/lib/essay/Chapters.svelte
+++ src/lib/essay/Chapters.svelte
@@ -1,7 +1,7 @@
-<div>
-	<div class="m-auto w-6/12">
-		<h1 class="pb-4 text-2xl md:text-5xl">The Hyper-visual Times</h1>
-		<div class="mb-20">
+<div class="w-full md:h-dvh">
+	<div class="md:w-3/7">
+		<h1 class="my-2 font-serif text-4xl md:text-2xl">The Hyper-visual Times</h1>
+		<p class="text-sm">
 			After building a partial blueprint of the material layer of news, the chapters included in
 			this interactive essay will dive into one particular dimension of digital news-making. The
 			goal is to reverse the transient and ephemeral nature of digital news through the creation of
@@ -9,66 +9,94 @@
 			digital news consumption with editorial and platform-related strategies coming to the
 			foreground in showing and comparing the rhetorical and contextual rules of selected news
 			outlets.
-		</div>
+		</p>
 	</div>
-	<div class="m-auto w-6/12">
-		<div class="mb-20">
-			<div class="my-4 cursor-pointer">
-				<h2 class="mb-2 font-serif text-xl">The geo-politics of news</h2>
-				<div class="flex cursor-pointer">
-					<div class="h-[25px] w-[25px] rounded-2xl bg-black px-2 text-center text-white">1</div>
-					<div class="ml-2 flex rounded-xl border px-2">
-						<div
-							class="mt-[5px] mr-[5px] h-[15px] w-[15px] animate-pulse rounded-xl bg-green-500"
-						></div>
-						Available
+	<div class="w-full md:grid md:h-2/3 md:grid-cols-3 md:grid-cols-7 md:gap-2 md:self-center">
+		<a
+			href="/places"
+			class="m-4 transition-all hover:-translate-y-[2px] md:col-span-2 md:col-start-2 md:m-0 md:mt-4 md:self-start"
+		>
+			<div class="bg-ivory-default rounded-xl border shadow-lg">
+				<div class="p-5">
+					<div class="flex justify-between">
+						<div>
+							<img src="img/places_header.svg" />
+						</div>
+						<div class="ml-2 flex items-center">
+							<div class="">
+								<div class="mr-[5px] h-[25px] w-[25px] animate-pulse rounded-xl bg-green-500"></div>
+							</div>
+							<div class="rounded-2xl border px-4 py-2">
+								<img src="icons/ui-forward.svg" />
+							</div>
+						</div>
 					</div>
+					<h2 class="my-2 font-serif text-lg">The geo-politics of news</h2>
+					<p>
+						This chapter focuses on how geographic keywords are used across the online coverage of
+						two newspapers: The New York Times and Zeit Online. First, keywords that are related to
+						geographic locations are extracted and preprocessed to reach a common standard for both
+						publications. Then, other keywords related to these countries are extracted to
+						investigate the overlaps and differences shown by the two newspapers in covering
+						geo-political issues.
+					</p>
 				</div>
-				<div class="my-2">
-					This chapter focuses on how geographic keywords are used across the online coverage of two
-					newspapers: The New York Times and Zeit Online. First, keywords that are related to
-					geographic locations are extracted and preprocessed to reach a common standard for both
-					publications. Then, other keywords related to these countries are extracted to investigate
-					the overlaps and differences shown by the two newspapers in covering geo-political issues.
-				</div>
-				<img class="m-auto my-2" src="img/places_header.svg" />
 			</div>
-			<div class="m-2 my-20">
-				<h2 class="mb-2 font-serif text-xl">Discoursive interrelations</h2>
-				<div class="flex">
-					<div class="h-[25px] w-[25px] rounded-2xl bg-black px-2 text-center text-white">2</div>
-					<div class="ml-2 flex rounded-xl border px-2">
-						<div class="mt-[5px] mr-[5px] h-[15px] w-[15px] rounded-xl bg-red-500"></div>
-						Coming soon
+		</a>
+		<a href="/" class="m-4 cursor-not-allowed md:col-span-2 md:m-0 md:mt-20 md:self-start">
+			<div class="bg-ivory-default rounded-xl border">
+				<div class="p-5">
+					<div class="flex justify-between">
+						<div>
+							<img src="img/texts_header.svg" />
+						</div>
+						<div class="ml-2 flex items-center">
+							<div class="">
+								<div class="mr-[5px] h-[25px] w-[25px] rounded-xl bg-red-500"></div>
+							</div>
+							<div class="rounded-2xl border px-4 py-2">
+								<img src="icons/ui-unavailable.svg" />
+							</div>
+						</div>
 					</div>
+					<h2 class="my-2 font-serif text-lg">Discoursive interrelations</h2>
+					<p>
+						Using machine learning and topic modeling techniques, key terms have been extracted from
+						one year worth of articles from The New York Times archives. As a result, we generated
+						clusters of similar words showing how often certain topics are discussed throughout the
+						newspaper coverage. In this chapter, these clusters have been visualized to allow for
+						analyzing the relationships between different themes.
+					</p>
 				</div>
-				<div class="my-2">
-					Using machine learning and topic modeling techniques, key terms have been extracted from
-					one year worth of articles from The New York Times archives. As a result, we generated
-					clusters of similar words showing how often certain topics are discussed throughout the
-					newspaper coverage. In this chapter, these clusters have been visualized to allow for
-					analyzing the relationships between different themes.
-				</div>
-				<img class="m-auto my-2" src="img/texts_header.svg" />
 			</div>
-			<div class="m-2 my-20">
-				<h2 class="mb-2 font-serif text-xl">The language of images</h2>
-				<div class="flex">
-					<div class="h-[25px] w-[25px] rounded-2xl bg-black px-2 text-center text-white">3</div>
-					<div class="ml-2 flex rounded-xl border px-2">
-						<div class="mt-[5px] mr-[5px] h-[15px] w-[15px] rounded-xl bg-red-500"></div>
-						Coming soon
+		</a>
+		<a href="/" class="m-4 cursor-not-allowed md:col-span-2 md:m-0 md:self-start">
+			<div class="bg-ivory-default rounded-xl border">
+				<div class="p-5">
+					<div class="flex justify-between">
+						<div>
+							<img src="img/images_header.svg" />
+						</div>
+						<div class="ml-2 flex items-center">
+							<div class="">
+								<div class="mr-[5px] h-[25px] w-[25px] rounded-xl bg-red-500"></div>
+							</div>
+							<div class="rounded-2xl border px-4 py-2">
+								<img src="icons/ui-unavailable.svg" />
+							</div>
+						</div>
 					</div>
+					<h2 class="my-2 font-serif text-lg">The language of images</h2>
+					<p>
+						This chapter focuses on images of people as cultural artefacts built using
+						long-established conventions of visual depictions. By extracting facial features from
+						one year Zeit Online and The New York Times coverage, only images depicting famous
+						people (politicians, actors, etc.) have been retained. The visualizations show how
+						frequently certain subjects are shown and what rhetorical devices are applied in
+						depicting them.
+					</p>
 				</div>
-				<div class="my-2">
-					This chapter focuses on images of people as cultural artefacts built using
-					long-established conventions of visual depictions. By extracting facial features from one
-					year Zeit Online and The New York Times coverage, only images depicting famous people
-					(politicians, actors, etc.) have been retained. The visualizations show how frequently
-					certain subjects are shown and what rhetorical devices are applied in depicting them.
-				</div>
-				<img class="m-auto my-2 w-92 grayscale" src="img/images_header.png" />
 			</div>
-		</div>
+		</a>
 	</div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,63 +4,110 @@
 	let isDropdownOpen = $state(false);
 
 	const handleDropdownClick = () => {
-    isDropdownOpen = !isDropdownOpen // togle state on click
-  }
+		isDropdownOpen = !isDropdownOpen; // togle state on click
+	};
 
-  const handleDropdownFocusLoss = (event: FocusEvent) => {
-	const { relatedTarget, currentTarget } = event;
-    // use "focusout" event to ensure that we can close the dropdown when clicking outside or when we leave the dropdown with the "Tab" button
-	if (relatedTarget instanceof HTMLElement 
-		&& currentTarget instanceof HTMLElement 
-		&& currentTarget.contains(relatedTarget)) return
-    
-	isDropdownOpen = false
-  }
+	const handleDropdownFocusLoss = (event: FocusEvent) => {
+		const { relatedTarget, currentTarget } = event;
+		// use "focusout" event to ensure that we can close the dropdown when clicking outside or when we leave the dropdown with the "Tab" button
+		if (
+			relatedTarget instanceof HTMLElement &&
+			currentTarget instanceof HTMLElement &&
+			currentTarget.contains(relatedTarget)
+		)
+			return;
+
+		isDropdownOpen = false;
+	};
 </script>
-<!-- <div class="block md:hidden">
-	<div class="m-auto w-8/12 h-dvh flex items-center">
-		<div>
-			<img class="w-1/12 max-h-full min-w-[50px] object-fit m-auto mb-6" src="img/pn-logo.png" alt="Panopticum News half circle logo"/>
-			<p class="text-center">This website is not ready (yet) for small viewport devices, please switch to a desktop computer or a tablet</p>
-		</div>
-	</div>
-</div> -->
-<div class="w-full max-w-7xl border-x m-auto">
+
+<div class="m-auto w-full max-w-7xl md:border-x">
 	<div class="sticky top-0 z-50">
-		<nav class="bg-ivory-default w-full p-4 border-b 2xl:border-l absolute">
+		<nav class="bg-ivory-default absolute w-full border-b p-4 2xl:border-l">
 			<div class="m-auto flex items-center justify-between">
 				<div class="w-2/3">
 					<a href="/">
 						<div class="flex w-full">
-							<img class="w-1/12 max-h-full min-w-[50px] object-scale-down" src="img/pn-logo.png" alt="Panopticum News half circle logo"/>
-							<span class="w-11/12 ml-2 self-center focus:italic text-lg">Panopticum News</span>
+							<img
+								class="max-h-full w-1/12 min-w-[50px] object-scale-down"
+								src="img/pn-logo.png"
+								alt="Panopticum News half circle logo"
+							/>
+							<span class="ml-2 w-11/12 self-center text-lg focus:italic">Panopticum News</span>
 						</div>
 					</a>
 				</div>
-				<div class="w-1/3 flex items-center justify-end">
+				<div class="flex w-1/3 items-center justify-end">
 					<div onfocusout={handleDropdownFocusLoss} class="relative inline-block text-left">
 						<div>
-							<button onclick={handleDropdownClick} type="button" class="w-full flex items-center justify-center border-none gap-x-1.5 pl-3 pr-0 text-sm cursor-pointer" id="menu-button" aria-expanded="true" aria-haspopup="true">
-								<span class="hidden md:block text-lg">Chapters</span>
+							<button
+								onclick={handleDropdownClick}
+								type="button"
+								class="flex w-full cursor-pointer items-center justify-center gap-x-1.5 border-none pr-0 pl-3 text-sm"
+								id="menu-button"
+								aria-expanded="true"
+								aria-haspopup="true"
+							>
+								<span class="hidden text-lg md:block">Chapters</span>
 								<span class="max-w-16">
-									<svg width="100%" height="30px" viewBox="0 0 46 21" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" data-slot="icon">
-										<circle cx="35" cy="10.5" r="10" fill="black" stroke="#fcf4e9" stroke-width="2"/>
+									<svg
+										width="100%"
+										height="30px"
+										viewBox="0 0 46 21"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+										aria-hidden="true"
+										data-slot="icon"
+									>
+										<circle
+											cx="35"
+											cy="10.5"
+											r="10"
+											fill="black"
+											stroke="#fcf4e9"
+											stroke-width="2"
+										/>
 										<text x="32" y="14" fill="#fcf4e9" font-size="10">3</text>
-										<circle cx="23" cy="10.5" r="10" fill="black" stroke="#fcf4e9"/>
+										<circle cx="23" cy="10.5" r="10" fill="black" stroke="#fcf4e9" />
 										<text x="20" y="14" fill="#fcf4e9" font-size="10">2</text>
-										<circle cx="11" cy="10.5" r="10" fill="black" stroke="#fcf4e9"/>
+										<circle cx="11" cy="10.5" r="10" fill="black" stroke="#fcf4e9" />
 										<text x="9" y="14" fill="#fcf4e9" font-size="10">1</text>
 									</svg>
-								</span>					
+								</span>
 							</button>
 						</div>
-						<div style:visibility={isDropdownOpen ? 'visible' : 'hidden'} class="absolute top-10 right-0 mt-2 z-10 mt-2 w-80  border bg-ivory-default rounded-md ring-1 ring-black/5 focus:outline-hidden" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-						<div class="py-1" role="none">
-							<!-- Active: "bg-gray-100 text-gray-900 outline-hidden", Not Active: "text-gray-700" -->
-							<a href="/places" class="block px-4 py-2 focus:italic hover:underline" role="menuitem" tabindex="-1" id="menu-item-0">Chapter 1: The Geo-politics of news</a>
-							<a href="/" class="block px-4 py-2 pointer-events-none cursor-not-allowed" role="menuitem" tabindex="-1" id="menu-item-1">Chapter 2: Discoursive interrelations</a>
-							<a href="/" class="block px-4 py-2 pointer-events-none cursor-not-allowed" role="menuitem" tabindex="-1" id="menu-item-2">Chapter 3: The language of images</a>
-						</div>
+						<div
+							style:visibility={isDropdownOpen ? 'visible' : 'hidden'}
+							class="bg-ivory-default absolute top-10 right-0 z-10 mt-2 mt-2 w-80 rounded-md border ring-1 ring-black/5 focus:outline-hidden"
+							role="menu"
+							aria-orientation="vertical"
+							aria-labelledby="menu-button"
+							tabindex="-1"
+						>
+							<div class="py-1" role="none">
+								<!-- Active: "bg-gray-100 text-gray-900 outline-hidden", Not Active: "text-gray-700" -->
+								<a
+									href="/places"
+									class="block px-4 py-2 hover:underline focus:italic"
+									role="menuitem"
+									tabindex="-1"
+									id="menu-item-0">Chapter 1: The Geo-politics of news</a
+								>
+								<a
+									href="/"
+									class="pointer-events-none block cursor-not-allowed px-4 py-2"
+									role="menuitem"
+									tabindex="-1"
+									id="menu-item-1">Chapter 2: Discoursive interrelations</a
+								>
+								<a
+									href="/"
+									class="pointer-events-none block cursor-not-allowed px-4 py-2"
+									role="menuitem"
+									tabindex="-1"
+									id="menu-item-2">Chapter 3: The language of images</a
+								>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -68,12 +115,6 @@
 		</nav>
 	</div>
 	<div class="-z-10">
-	{@render children()}
+		{@render children()}
 	</div>
-	<footer class="sticky bottom-0 bg-ivory-default w-full px-4 pb-2 border-t 2xl:border-l z-40 mt-10">
-		<!-- <button class="mt-2">
-			<span>Scroll back to top</span>
-			<img class="rotate-180 inline ml-2" src="icons/ui-scroll.svg" alt="Arrow pointing upwards"/>
-		</button> -->
-	</footer>
 </div>
