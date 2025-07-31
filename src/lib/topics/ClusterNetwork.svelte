d4daa2712d1d1136e2f2147149a4e8282ebc899e
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-07-31 15:45:07 +0200

    feat: data filter on cluster selection, close #27

--- src/lib/topics/ClusterNetwork.svelte
+++ src/lib/topics/ClusterNetwork.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import NetworksData from '../../content/data/topics/networks.json';
 	let { selectedCluster, switchView } = $props();
 	let width = $state(0);
 	let height = $state(0);
+	let currentNetworkData = $derived(
+		NetworksData.filter((network) => {
+			return selectedCluster[0].id === network.topic;
+		})
+	);
+	$inspect(currentNetworkData);
 
 	function handleClusterReset() {
 		switchView({
