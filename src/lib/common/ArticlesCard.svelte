8d3d44003d67d5b9c47754035ecc4afd3336c13c
elproffesore <philipp.proff@gmx.de>
2025-10-16 12:44:03 +0200

    add more generically useable components

---
+++ src/lib/common/ArticlesCard.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { get } from 'svelte/store';
+	import translateMap from '../../content/data/images/translate_map.json';
+	let { articles, currentSource, selectedPerson, selectedDate, x, y } = $props();
+	function getAuthorSourceAgnostic(byline){
+		 if (currentSource == 'NYT'){
+			let by = JSON.parse(JSON.parse(byline))
+			if (by.original.length == 0) return 'Author unknown'
+			return by.original ?? 'Author unknown'
+		 }else{		
+			let by = JSON.parse(byline)
+			if (!by || by.length == 0) return 'Author unknown'
+			return by[0].name ?? 'Author unknown'
+		 }
+	}
+</script>
+
+<div
+	class=" absolute h-60 w-[300px] overflow-scroll overscroll-none rounded-xl border border-black backdrop-blur-lg md:h-80 translate-x-[75px] translate-y-[-125px]" style:top={y+"px"} style:left={x+"px"}>
+	<div class="sticky top-0 flex justify-between p-2 backdrop-blur-sm shadow-md bg-[var(--color-ivory-default)]">
+		<div class="grid grid-cols-2 pt-1">
+			<div class="pr-2 col-span-2 font-bold">
+				{translateMap[selectedPerson]} 
+			</div>
+				<div class="pr-2 col-span-2">
+					({new Date(selectedDate).toLocaleDateString('de')} - {new Date(new Date(selectedDate).getTime() + 7 * 24 * 60 * 60 * 1000).toLocaleDateString('de')})
+					<img class=" inline" src="icons/ui-forward.svg" alt="arrow right" />
+					 <u>{articles.length}</u> Articles 
+				</div>
+		</div>
+	</div>
+	<div class="px-2">
+		{#each articles as article}
+			<div class="bg-[var(--color-ivory-default)] my-3 rounded-xl border border-black p-2">
+				<a href={article.web_url} target="_blank">
+					<div>
+						<div>{article.headline}</div>
+						<div class="py-2 text-sm">{article.snippet}</div>
+						<div class="text-xs">
+							{getAuthorSourceAgnostic(article.byline)} |
+							{new Date(article.pub_date).toLocaleDateString('de')}
+						</div>
+					</div>
+				</a>
+			</div>
+		{/each}
+	</div>
+</div>
---
+++ src/lib/common/BackwardButton.svelte
@@ -0,0 +1,18 @@
+<script>
+    let {backto, back = $bindable()} = $props()
+    function handleBack(){
+        console.log(back)
+        if (!back){
+            back = true;
+        }
+    }
+</script>
+<button onclick={handleBack}
+	><span class="mr-1"
+		><img
+			src="./icons/ui-forward.svg"
+			class="inline-block align-middle rotate-180"
+			alt="arrow backward"
+		/></span>
+    Back to {backto}
+</button>
