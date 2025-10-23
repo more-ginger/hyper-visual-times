70f81980340f96027d90baf07a44334cd6b11427
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-10-23 10:26:49 +0200

    feat: html structure for about page

---
+++ src/lib/main/About.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+</script>
+
+<div class="flex w-full">
+	<div class="w-3/5 bg-red-100">
+		<div>
+			<h2>About the project</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tellus metus, accumsan
+				quis rutrum sed, viverra ac mi. Cras eget nisi orci. Suspendisse lectus lorem, molestie ac
+				libero vitae, pellentesque volutpat enim. Fusce eu sodales nibh. Maecenas hendrerit est non
+				dolor hendrerit faucibus. In sed ligula a eros malesuada ultrices a vitae felis. Class
+				aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce
+				venenatis leo mauris, non tincidunt magna tristique vitae. Vivamus euismod, nisi vel rhoncus
+				viverra, purus nisl congue nulla, et elementum neque est in odio. Quisque faucibus vitae
+				diam id vestibulum. Suspendisse viverra consequat ligula, rhoncus tincidunt nisl mollis et.
+				Sed elementum suscipit venenatis. Nullam tristique bibendum massa, eget volutpat magna
+				egestas nec. Aenean ullamcorper risus nec eros aliquam bibendum. Maecenas felis turpis,
+				ultrices sed lectus eget, accumsan malesuada dolor. Pellentesque ornare facilisis neque ac
+				aliquam. Etiam augue felis, convallis in neque et, aliquam blandit tortor. Nullam purus
+				nulla, euismod consequat ullamcorper eget, luctus id dui. Nam ut eros sem. Proin justo quam,
+				condimentum eget dictum eget, convallis id erat.
+			</p>
+		</div>
+		<div>
+			<h2>Team</h2>
+			<div>
+				<div>
+					<p>Francesca Morini</p>
+					<p>Principal Investigator</p>
+				</div>
+				<div>
+					<p>Philipp Proff</p>
+					<p>Information Design</p>
+				</div>
+				<div>
+					<p>Angela Brennecke</p>
+					<p>Project Management</p>
+				</div>
+				<div>
+					<p>Jocelyn Shek</p>
+					<p>Data Analysis</p>
+				</div>
+				<div>
+					<p>Nemuël Link</p>
+					<p>Data Analysis and Design</p>
+				</div>
+				<div>
+					<p>Kim Albrecht</p>
+					<p>Research Supervision</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="w-2/5 bg-green-100">
+		<div>
+			<h2>Fundings & Partners</h2>
+			<div>Logo BMBF</div>
+			<div>Logo Filmuni</div>
+			<div>Logo difgl</div>
+		</div>
+		<div>
+			<h2>Related links</h2>
+			<div>Documentation</div>
+			<div>Github repository</div>
+			<div>Impressum</div>
+		</div>
+	</div>
+</div>
