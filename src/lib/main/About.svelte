f7aae8c4f95ee83ec58dadc5cca6e4bb9585ab96
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-10-23 11:00:45 +0200

    feat: photo tiles

--- src/lib/main/About.svelte
+++ src/lib/main/About.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class="flex w-full">
+<div class="m-auto flex w-4/5">
 	<div class="w-3/5 bg-red-100">
 		<div>
 			<h2>About the project</h2>
@@ -24,30 +24,48 @@
 		</div>
 		<div>
 			<h2>Team</h2>
-			<div>
-				<div>
-					<p>Francesca Morini</p>
-					<p>Principal Investigator</p>
+			<div class="grid grid-cols-3">
+				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
+					<img class="h-[250px] w-96 object-cover" src="img/team/fm_team.jpg" />
+					<div class="absolute bottom-0 z-50 w-full bg-white p-2">
+						<p>Francesca Morini</p>
+						<p class="text-xs">Principal Investigator</p>
+					</div>
 				</div>
-				<div>
-					<p>Philipp Proff</p>
-					<p>Information Design</p>
+				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
+					<img class="h-[250px] w-96 object-cover" src="img/team/pp_team.webp" />
+					<div class="absolute bottom-0 z-50 w-full bg-white p-2">
+						<p>Philipp Proff</p>
+						<p class="text-xs">Information Design</p>
+					</div>
 				</div>
-				<div>
-					<p>Angela Brennecke</p>
-					<p>Project Management</p>
+				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
+					<img class="h-[250px] w-96 object-cover" src="img/team/ab_team.jpeg" />
+					<div class="absolute bottom-0 z-50 w-full bg-white p-2">
+						<p>Angela Brennecke</p>
+						<p class="text-xs">Project Management</p>
+					</div>
 				</div>
-				<div>
-					<p>Jocelyn Shek</p>
-					<p>Data Analysis</p>
+				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
+					<img class="h-[250px] w-96 object-cover" src="img/team/js_team.jpeg" />
+					<div class="absolute bottom-0 z-50 w-full bg-white p-2">
+						<p>Jocelyn Shek</p>
+						<p class="text-xs">Data Analysis</p>
+					</div>
 				</div>
-				<div>
-					<p>Nemuël Link</p>
-					<p>Data Analysis and Design</p>
+				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
+					<img class="h-[250px] w-96 object-cover" src="img/placeholder-nyt.png" />
+					<div class="absolute bottom-0 z-50 w-full bg-white p-2">
+						<p>Nemuël Link</p>
+						<p class="text-xs">Data Analysis and Design</p>
+					</div>
 				</div>
-				<div>
-					<p>Kim Albrecht</p>
-					<p>Research Supervision</p>
+				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
+					<img class="h-[250px] w-96 object-cover" src="img/team/ka_team.jpg" />
+					<div class="absolute bottom-0 z-50 w-full bg-white p-2">
+						<p>Kim Albrecht</p>
+						<p class="text-xs">Research Supervision</p>
+					</div>
 				</div>
 			</div>
 		</div>
