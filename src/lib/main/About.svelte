9ed2fad84d8e7de8950a9b3afe7c4fdb8a85a577
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-10-23 11:36:23 +0200

    style: changing spacing and icons for text blocks and related links

--- src/lib/main/About.svelte
+++ src/lib/main/About.svelte
@@ -5,7 +5,7 @@
 	<div class="w-3/5">
 		<div>
 			<h2 class="font-serif text-2xl">About the project</h2>
-			<p>
+			<p class="mt-8">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tellus metus, accumsan
 				quis rutrum sed, viverra ac mi. Cras eget nisi orci. Suspendisse lectus lorem, molestie ac
 				libero vitae, pellentesque volutpat enim. Fusce eu sodales nibh. Maecenas hendrerit est non
@@ -22,46 +22,70 @@
 				condimentum eget dictum eget, convallis id erat.
 			</p>
 		</div>
-		<div>
+		<div class="mt-10">
 			<h2 class="font-serif text-2xl">Team</h2>
-			<div class="grid grid-cols-3">
+			<div class="mt-8 grid grid-cols-3">
 				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
-					<img class="h-[250px] w-96 object-cover" src="img/team/fm_team.jpg" />
+					<img
+						class="h-[250px] w-96 object-cover"
+						src="img/team/fm_team.jpg"
+						alt="Portrait of Francesca Morini"
+					/>
 					<div class="absolute bottom-0 z-50 w-full border-t bg-white p-2">
 						<p>Francesca Morini</p>
 						<p class="text-xs">Principal Investigator, design and development</p>
 					</div>
 				</div>
 				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
-					<img class="h-[250px] w-96 object-cover" src="img/team/pp_team.webp" />
+					<img
+						class="h-[250px] w-96 object-cover"
+						src="img/team/pp_team.webp"
+						alt="Portrait of Philipp Proff"
+					/>
 					<div class="absolute bottom-0 z-50 w-full border-t bg-white p-2">
 						<p>Philipp Proff</p>
 						<p class="text-xs">Researcher, design and development</p>
 					</div>
 				</div>
 				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
-					<img class="h-[250px] w-96 object-cover" src="img/team/ab_team.jpeg" />
+					<img
+						class="h-[250px] w-96 object-cover"
+						src="img/team/ab_team.jpeg"
+						alt="Portrait of Angela Brennecke"
+					/>
 					<div class="absolute bottom-0 z-50 w-full border-t bg-white p-2">
 						<p>Angela Brennecke</p>
 						<p class="text-xs">Professor, project management and administration</p>
 					</div>
 				</div>
 				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
-					<img class="h-[250px] w-96 object-cover" src="img/team/js_team.jpeg" />
+					<img
+						class="h-[250px] w-96 object-cover"
+						src="img/team/js_team.jpeg"
+						alt="Portrait of Jocelyn Shek"
+					/>
 					<div class="absolute bottom-0 z-50 w-full border-t bg-white p-2">
 						<p>Jocelyn Shek</p>
 						<p class="text-xs">Student researcher, Data Analysis</p>
 					</div>
 				</div>
 				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
-					<img class="h-[250px] w-96 object-cover" src="img/placeholder-nyt.png" />
+					<img
+						class="h-[250px] w-96 object-cover"
+						src="img/placeholder-nyt.png"
+						alt="Portrait of Nemuël Link"
+					/>
 					<div class="absolute bottom-0 z-50 w-full border-t bg-white p-2">
 						<p>Nemuël Link</p>
 						<p class="text-xs">Student researcher, Data Analysis and Design</p>
 					</div>
 				</div>
 				<div class="relative m-2 max-h-[250px] overflow-hidden rounded-xl border bg-white">
-					<img class="h-[250px] w-96 object-cover" src="img/team/ka_team.jpg" />
+					<img
+						class="h-[250px] w-96 object-cover"
+						src="img/team/ka_team.jpg"
+						alt="Portrait of Kim Albrecht"
+					/>
 					<div class="absolute bottom-0 z-50 w-full border-t bg-white p-2">
 						<p>Kim Albrecht</p>
 						<p class=" text-xs">Professor, Research Supervision and administration</p>
@@ -70,32 +94,38 @@
 			</div>
 		</div>
 	</div>
-	<div class="w-2/5 pl-4">
+	<div class="w-2/5 pl-12">
 		<div>
 			<h2 class="font-serif text-2xl">Fundings & Partners</h2>
-			<div><img class="w-[250px]" src="/img/partners/BMFTR.jpg" /></div>
-			<div><img class="w-[200px]" src="/img/partners/Filmuni.png" /></div>
-			<div><img class="w-[150px]" src="/img/partners/difgl_logo.jpeg" /></div>
+			<div class="mt-8">
+				<div><img class="w-[250px]" src="/img/partners/BMFTR.jpg" alt="BMFTR Logo" /></div>
+				<div>
+					<img class="w-[200px]" src="/img/partners/Filmuni.png" alt="Film University Logo" />
+				</div>
+				<div><img class="w-[150px]" src="/img/partners/difgl_logo.jpeg" alt="DIFGL Logo" /></div>
+			</div>
 		</div>
-
-		<div>
+		<div class="mt-10">
 			<h2 class="font-serif text-2xl">Related links</h2>
-			<div>
+			<div class="mt-8">
 				<ul>
 					<li class="pb-4">
 						<button class="flex items-center px-2 py-[2px]">
-							<a href="/places">Documentation</a></button
-						>
+							<a href="/places">Documentation</a>
+							<span class="ml-2"><img src="icons/ui-forward.svg" alt="Forward arrow icon" /></span>
+						</button>
 					</li>
 					<li class="pb-4">
 						<button class="flex items-center px-2 py-[2px]">
-							<a href="/topics">Github repository</a></button
-						>
+							<a href="/topics">Github repository</a>
+							<span class="ml-2"><img src="icons/ui-forward.svg" alt="Forward arrow icon" /></span>
+						</button>
 					</li>
 					<li class="pb-4">
 						<button class="pointer-events-none flex items-center px-2 py-[2px]">
-							<a href="/images">Impressum</a></button
-						>
+							<a href="/images">Impressum</a>
+							<span class="ml-2"><img src="icons/ui-forward.svg" alt="Forward arrow icon" /></span>
+						</button>
 					</li>
 				</ul>
 			</div>
