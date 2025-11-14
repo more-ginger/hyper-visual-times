fbe222178cbc13e2fe3cb900971898433135aae6
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-11-14 12:05:36 +0100

    style: add alt text to OuterSelector

--- src/lib/common/OutletSelector.svelte
+++ src/lib/common/OutletSelector.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
-	import { selectedOutlet} from '$lib/utils/state.images.svelte.ts';
+	import { selectedOutlet } from '$lib/utils/state.images.svelte.ts';
 </script>
-<div class="flex justify-center p-2 lg:justify-end lg:p-0 bg-[var(--color-ivory-default)] w-max z-50">
+
+<div
+	class="z-50 flex w-max justify-center bg-[var(--color-ivory-default)] p-2 lg:justify-end lg:p-0"
+>
 	<div class="m-2 grow">
 		<button
-			class={ "flex gap-2 " + ($selectedOutlet == 'Zeit' ? `bg-zeit-light` : `bg-default`)}
+			class={'flex gap-2 ' + ($selectedOutlet == 'Zeit' ? `bg-zeit-light` : `bg-default`)}
 			onclick={() => {
 				selectedOutlet.set('Zeit');
-			}}><img src="icons/ui-interact.svg" class="max-w-min"/>Zeit Online</button
+			}}
+			><img src="icons/ui-interact.svg" class="max-w-min" alt="Switch to Zeit Online" />Zeit Online</button
 		>
 	</div>
 	<img class="inline max-w-[20px]" src="icons/ui-switch.svg" alt="Switch with The New York Times" />
 	<div class="m-2 grow">
 		<button
-			class={"flex gap-2 " + ($selectedOutlet == 'NYT' ? `bg-nyt-light` : `bg-default`)}
+			class={'flex gap-2 ' + ($selectedOutlet == 'NYT' ? `bg-nyt-light` : `bg-default`)}
 			onclick={() => {
 				selectedOutlet.set('NYT');
-			}}><img src="icons/ui-interact.svg" class="max-w-min"/>The New York Times</button
+			}}
+			><img src="icons/ui-interact.svg" class="max-w-min" alt="Switch to The New York Times" />The
+			New York Times</button
 		>
 	</div>
 </div>
