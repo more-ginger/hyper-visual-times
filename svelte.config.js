a20c3d08aa254ec9bcf904c0a2732cce2bba1089
philipp <51952239+elproffesore@users.noreply.github.com>
2025-11-25 11:03:16 +0100

    feat: make the site deployable on Github Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 // adapter to deploy on Netlify
-import adapter from '@sveltejs/adapter-netlify';
+//import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,19 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+			
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
